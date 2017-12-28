2460621dc038222bab9daf406d416c2dfb7c48d6
Kai Pohl <shiro@coopanionts.ddns.net>
2017-12-28 16:50:03 +0100

    Improvements were made
    Split the Telegram bot parts and the helping functions in two separate
    scripts and added two new functions: kiss and slap (for when someone
    tries to kiss you, you can show him that you don't like it).
    Also added functionality to throw snowballs by simply reply to someone.
    This also works with kiss and slap.

---
+++ emote2mebot.py
@@ -0,0 +1,83 @@
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.error import (TelegramError, Unauthorized, BadRequest, TimedOut, ChatMigrated, NetworkError)
+from ircmefuncs import (linkedUser, getUser, getArgs, snowballText, kissText, slapText)
+
+updater = Updater(token='')
+dispatcher = updater.dispatcher
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+def start(bot, update):
+	bot.send_message(chat_id=update.message.chat_id, text="Hello buddy! I am here to give your group chat some possibilities for more action. Just invite me to any group and then type /me and what you want to virtually do and I will do the magic.\nFor example you can type '/me invited this bot' and I will change it to 'Bob invited this bot.' (in case your name is Bob).\nJust try it out!")
+
+def me(bot, update):
+	theMe = linkedUser(getUser(update.message))
+	theArgs = getArgs(update.message)
+	bot.send_message(chat_id=update.message.chat_id, text=u'{0} {1}'.format(theMe, theArgs), parse_mode = 'Markdown')
+	bot.delete_message(chat_id=update.message.chat_id, message_id=update.message.message_id)
+
+def snowball(bot, update):
+	realText = snowballText(update.message)
+	if realText is not None:
+		bot.send_message(chat_id=update.message.chat_id, text=realText, parse_mode = 'Markdown')
+	rmMessage(bot, update.message)
+
+def kiss(bot, update):
+        realText = kissText(update.message)
+        if realText is not None:
+                bot.send_message(chat_id=update.message.chat_id, text=realText, parse_mode = 'Markdown')
+	rmMessage(bot, update.message)
+
+def slap(bot, update):
+        realText = slapText(update.message)
+        if realText is not None:
+                bot.send_message(chat_id=update.message.chat_id, text=realText, parse_mode = 'Markdown')
+	rmMessage(bot, update.message)
+
+def cmds(bot, update):
+        bot.send_message(chat_id = update.message.chat_id, text = 'Available commands:\n/me [text]\n/snowball [name/mention]\n/kiss [name/mention]\n/slap [name/mention]\n\nAll commands except /me can also be triggered by replying to any other message.')
+
+def rmMessage(bot, message):
+	if  message.chat.type != "private":
+		try:
+			bot.delete_message(chat_id=message.chat_id, message_id=message.message_id)
+		except BadRequest:
+			print("There is a group I am no admin in.")
+
+
+def error_callback(bot, update, error):
+	try:
+		raise error
+	except Unauthorized:
+		print ('UnauthorizedError >> ' + str(error))
+		# remove update.message.chat_id from conversation list
+	except BadRequest:
+		print ('BadRequestError >> ' + str(error))
+		# handle malformed requests - read more below!
+	except TimedOut:
+		print ('TimedOutError >> ' + str(error))
+		# handle slow connection problems
+	except NetworkError:
+		print ('NetworkError >> ' + str(error))
+		# handle other connection problems
+	except ChatMigrated as e:
+		print ('ChatMigratedError >> ' + str(error))
+		# the chat_id of a group has changed, use e.new_chat_id instead
+	except TelegramError:
+		print ('AnotherError >> ' + str(error))
+		# handle all other telegram related errors
+
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('me', me))
+dispatcher.add_handler(CommandHandler('snowball', snowball))
+dispatcher.add_handler(CommandHandler('kiss', kiss))
+dispatcher.add_handler(CommandHandler('slap', slap))
+dispatcher.add_handler(CommandHandler('cmds', cmds))
+dispatcher.add_handler(CommandHandler('commands', cmds))
+dispatcher.add_handler(CommandHandler('help', cmds))
+dispatcher.add_error_handler(error_callback)
+updater.start_polling()
+
+updater.idle()
+
---
+++ ircmefuncs.py
@@ -0,0 +1,84 @@
+from random import randint
+
+def getUser(message):
+        return message.from_user
+
+def linkedUser(user):
+        return u'[{}](tg://user?id={})'.format(user['first_name'], user['id'])
+
+def snowballText(message):
+	scenario = randint(0,100)
+	theMe = linkedUser(getUser(message))
+	theArgs = checkMessage(message)
+	if theArgs is "":
+		return None
+	if scenario > 80:
+		return u'{0} sees that {1} is preparing a snowball and throws first, disarming {1} again!'.format(theMe, theArgs)
+	if scenario > 50:
+		return u'{0} aims carefully at {1}\'s chest, throws, and the snowball hits exactly there!'.format(theMe, theArgs)
+	if scenario > 40:
+		return u'{0} throws a snowball and accidentally hits {1} in the face. Not cool!'.format(theMe, theArgs)
+	if scenario > 10:
+		return u'{0} throws a snowball to {1} but it hits the ground in front of {1}!'.format(theMe, theArgs)
+	if scenario > 0:
+		return u'{0} tries to throw a snowball at {1} but instead slips and lands on the butt!'.format(theMe, theArgs)
+	return u'{0} throws a snowball at {1} but {1} catches and throws it back to {0}! Better be faster next time.'.format(theMe, theArgs)
+
+def kissText(message):
+	scenario = randint(0,100)
+	theMe = linkedUser(getUser(message))
+	theArgs = checkMessage(message)
+	if theArgs is "":
+		return None
+	if scenario > 75:
+		return u'{0} pushes {1} softly on the wall and they start kissing long and wild.'.format(theMe, theArgs)
+	if scenario > 50:
+		return u'{0} looks into {1} eyes deeply and they kiss each other softly.'.format(theMe, theArgs)
+	if scenario > 25:
+		return u'{1} sees that {0} is coming closer and blushes when {0} kisses {1}.'.format(theMe, theArgs)
+	if scenario > 10:
+		return u'{0} sees {1} smiling and decides to give {1} a quick kiss.'.format(theMe, theArgs)
+	return u'{0} gives {1} a sweet kiss on the cheek.'.format(theMe, theArgs)
+
+def slapText(message):
+	scenario = randint(0,100)
+	theMe = linkedUser(getUser(message))
+	theArgs = checkMessage(message)
+	if theArgs is "":
+		return None
+	if scenario > 75:
+		return u'{0} is enraged about {1} and punches {1} in the face, flying into the next wall.'.format(theMe, theArgs)
+	if scenario > 50:
+		return u'{0} slaps {1} as hard as possible in the face. The handprint is still visible on the cheek.'.format(theMe, theArgs)
+	if scenario > 25:
+		return u'{0} is angry about {1} and slaps {1} in the face.'.format(theMe, theArgs)
+	if scenario > 10:
+		return u'{0} shoves {1} angrily against the wall.'.format(theMe, theArgs)
+	return u'{0} tries to slap {1} but {1} predicted it and turns it into a high five.'.format(theMe, theArgs)
+
+def getArgs(message):
+	firstElem = message.entities[0].length + 1
+	messageText = ''
+	for i in message.entities:
+		lastElem = i.offset
+		if i.user is not None:
+			messageText = messageText + message.text[firstElem:lastElem] + linkedUser(i.user)
+			firstElem = i.offset+i.length
+	if firstElem < len(message.text):
+		messageText = messageText + message.text[firstElem:]
+	return messageText
+
+def getMention(message):
+	for i in message.entities:
+		if i.type == 'text_mention':
+			return linkedUser(i.user)
+		if i.type == 'mention':
+			return message.text[i.offset:i.offset+i.length]
+	return None
+
+def checkMessage(message):
+	if message.reply_to_message is not None:
+		return linkedUser(getUser(message.reply_to_message))
+	elif getMention(message) is not None:
+		return getMention(message)
+	return getArgs
