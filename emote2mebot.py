3bee39da0a772d40ad2fda8c4456bf1c7fa0b1de
Kai Pohl <shiro@coopanionts.ddns.net>
2017-12-31 13:29:26 +0100

    Change something
    Some changes were made, don't know which one.. but it works.

--- emote2mebot.py
+++ emote2mebot.py
@@ -1,9 +1,9 @@
 from telegram.ext import CommandHandler
 from telegram.error import (TelegramError, Unauthorized, BadRequest, TimedOut, ChatMigrated, NetworkError)
 from ircmefuncs import (linkedUser, getUser, getArgs, snowballText, kissText, slapText)
-from bottoken import getToken
+import bottoken
 
-updater = getToken()
+updater = bottoken.getToken()
 dispatcher = updater.dispatcher
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
